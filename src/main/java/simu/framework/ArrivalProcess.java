1065eb7965405a2046b1788eda1e18c6172853cf
Trung Dinh Vu <trungv@metropolia.fi>
2024-12-03 05:34:11 +0200

    update successfully connect the system

---
+++ src/main/java/simu/framework/ArrivalProcess.java
@@ -0,0 +1,22 @@
+package simu.framework;
+
+import eduni.distributions.ContinuousGenerator;
+
+public class ArrivalProcess {
+	private ContinuousGenerator generator;
+	private EventList eventList;
+	private IEventType type;
+
+	public ArrivalProcess(ContinuousGenerator g, EventList tl, IEventType type) {
+		this.generator = g;
+		this.eventList = tl;
+		this.type = type;
+	}
+
+	public void generateNextEvent() {
+		System.out.println("In arrival process .. generating events");
+		Event t = new Event(type, Clock.getInstance().getClock() + generator.sample());
+		System.out.println("Event Type " + t.getType());
+		eventList.add(t);
+	}
+}
---
+++ src/main/java/simu/model/ServicePoint.java
@@ -0,0 +1,139 @@
+package simu.model;
+
+import eduni.distributions.ContinuousGenerator;
+import simu.framework.*;
+
+import java.util.LinkedList;
+
+// TODO:
+// Service Point functionalities & calculations (+ variables needed) and reporting to be implemented
+public class ServicePoint {
+    private LinkedList<Customer> queue = new LinkedList<>(); // Data Structure used
+    private ContinuousGenerator generator;
+    private EventList eventList;
+    private EventType eventTypeScheduled;
+    //Queuestrategy strategy; // option: ordering of the customer
+    private boolean reserved = false;
+    private boolean isActive;
+    private double totalServiceTime = 0;
+    private int totalCustomersServed = 0;
+    private int totalCustomersRemoved = 0;
+    private double totalWaitingTime = 0;
+
+
+    public ServicePoint(ContinuousGenerator generator, EventList eventList, EventType type, boolean isActive) {
+        this.eventList = eventList;
+        this.generator = generator;
+        this.eventTypeScheduled = type;
+        this.isActive = isActive;
+    }
+
+    public ServicePoint(ContinuousGenerator generator, EventList eventList, EventType type) {
+        this(generator, eventList, type, true); // Default active to true
+    }
+
+    // remove repeat
+    public static double getAverageServiceTime(ServicePoint[] servicePoints) {
+        double totalServiceTime = 0;
+        int totalCustomersRemoved = 0;
+        for (ServicePoint sp : servicePoints) {
+            totalServiceTime += sp.totalServiceTime;
+            totalCustomersRemoved += sp.totalCustomersRemoved;
+        }
+        return totalCustomersRemoved == 0 ? 0 : totalServiceTime / totalCustomersRemoved;
+    }
+
+    public static double getAverageWaitingTime(ServicePoint[] servicePoints) {
+        double totalWaitingTime = 0;
+        int totalCustomersRemoved = 0;
+        for (ServicePoint sp : servicePoints) {
+            totalWaitingTime += sp.totalWaitingTime;
+            totalCustomersRemoved += sp.totalCustomersRemoved;
+        }
+        return totalCustomersRemoved == 0 ? 0 : totalWaitingTime / totalCustomersRemoved;
+    }
+
+    public int getQueueSize() {
+        return queue.size();
+    }
+
+    public void addQueue(Customer a) {    // The first customer of the queue is always in service
+        if (isActive) {
+            queue.add(a);
+            totalCustomersServed++;
+        }
+    }
+
+    public Customer removeQueue() {        // Remove serviced customer
+        reserved = false;
+        Customer customer = queue.poll();
+        if (customer != null) {
+            Trace.out(Trace.Level.INFO, "ServicePoint: Customer #" + customer.getId() + " removed from the queue");
+            double serviceTime = Clock.getInstance().getClock() - customer.getArrivalTime();
+            double waitingTime = customer.getServiceStartTime() - customer.getArrivalTime();
+            totalServiceTime += serviceTime;
+            totalWaitingTime += waitingTime;
+            totalCustomersRemoved++;
+        }
+        return customer;
+    }
+
+//    public void beginService() { // Begins a new service, customer is on the queue during the service
+//        Customer customer = queue.peek();
+//        if (customer != null) {
+//            customer.setServiceStartTime(Clock.getInstance().getClock());
+//            Trace.out(Trace.Level.INFO, "ServicePoint: Starting a new service for the customer #" + customer.getId());
+//            reserved = true;
+//            double serviceTime = generator.sample();
+//            customer.setExpectedDepartureTime(Clock.getInstance().getClock() + serviceTime);
+//            eventList.add(new Event(eventTypeScheduled, Clock.getInstance().getClock() + serviceTime));
+//        }
+//    }
+
+    public Customer beginService() {
+        Customer customer = queue.peek();
+        if (customer != null) {
+            customer.setServiceStartTime(Clock.getInstance().getClock());
+            Trace.out(Trace.Level.INFO, "ServicePoint: Starting a new service for the customer #" + customer.getId());
+            reserved = true;
+            double serviceTime = generator.sample();
+            customer.setExpectedDepartureTime(Clock.getInstance().getClock() + serviceTime);
+            eventList.add(new Event(eventTypeScheduled, Clock.getInstance().getClock() + serviceTime));
+        }
+        return customer;
+    }
+
+    public Customer peekQueue() {
+        return queue.peek();
+    }
+
+    public boolean isReserved() {
+        return reserved;
+    }
+
+    public boolean isOnQueue() {
+        return queue.size() != 0;
+    }
+
+    public boolean isActive() {
+        return isActive;
+    }
+
+    public void setActive(boolean active) {
+        this.isActive = active;
+    }
+
+    public double getAverageServiceTime() {
+        return totalCustomersRemoved == 0 ? 0 : totalServiceTime / totalCustomersRemoved;
+    }
+
+    public double getAverageWaitingTime() {
+        return totalCustomersRemoved == 0 ? 0 : totalWaitingTime / totalCustomersRemoved;
+    }
+
+    public int getTotalCustomersRemoved() {
+        return totalCustomersRemoved;
+    }
+
+
+}
